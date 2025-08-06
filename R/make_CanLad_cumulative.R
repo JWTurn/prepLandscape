535858b501592d330ba09cc479830616b38dbd69
Julie Turner <julwturner@gmail.com>
2025-08-06 12:02:45 -0700

    init make_CanLad_cumulative

---
+++ R/make_CanLad_cumulative.R
@@ -0,0 +1,20 @@
+#' @title make cumulative CanLaD disturbance per year per disturbance type
+#' @export
+#' @author Julie W. Turner
+makeCanLadCumulative <- function(yrs, disturbTypeCode, dPath, rtm){
+  # make a list of the new harvests, make a spatRasterCollection, and mosaic
+  mosaic(sprc(
+    Map(yr = yrs, disturbType = disturbTypeCode, function(yr, disturbType){
+      disturbYr <- reproducible::prepInputs(url = paste0('https://ftp.maps.canada.ca/pub/nrcan_rncan/Forests_Foret/canlad_including_insect_defoliation/v1/Disturbances_Time_Series/canlad_annual_', 
+                                                         yr, '_v1.tif'),
+                                            destinationPath = dPath,
+                                            fun = 'terra::rast',
+                                            to = rtm,
+                                            method = 'near')
+      indivDisturbYr <- (match(disturbYr, disturbType))*yr
+      names(indivDisturbYr) <- as.character(yr)
+      return(indivDisturbYr)
+      message(paste0('completed ', yr))
+      
+    })))
+}
