6ef2e6fe5aadc24d84684ca4e8cfed91068d1d3d
Julie Turner <julwturner@gmail.com>
2025-08-15 15:21:31 -0700

    init make_timeSinceDisturb_rast

---
+++ R/make_timeSinceDisturb_rast.R
@@ -0,0 +1,34 @@
+#' @title make rasters of most recent disturbance by year
+#' @export
+#' @author Julie W. Turner
+#' 
+#' 
+make_timeSinceDisturb_rast <- function(layer, rast, disturbanceType, minyr = NULL, backgrnd, where2save = NULL){
+  lyr <- layer
+  raster <- rast
+  yearsWithData <- unique(lyr[["YEAR"]])$YEAR
+  if(!is.null(minyr)){
+    yearsWithData <- minyr:max(yearsWithData, na.rm = T)
+  }
+  
+  timeSinceYearly <- Map(yrName = paste0('year', yearsWithData),yr = yearsWithData, function(yrName, yr) {
+    subst <- subset(lyr, lyr$YEAR<= yr)
+    print(paste0('start ', yrName))
+    #fires <- 
+    suppressWarnings({
+      historicalRasters <- rasterize(subst, raster, field="YEAR", fun=max, background = backgrnd)
+      timeSince <- yr - mask(historicalRasters, raster)
+      
+      names(timeSince) <- paste0('timeSince', disturbanceType)
+    })
+    
+    if(!is.null(where2save)){
+      fnames <- paste0("ts_", disturbanceType, "_", yr, ".tif")
+      writeRaster(timeSince, file.path(where2save, fnames), overwrite = T)
+      print(paste0('saved ', disturbanceType, ' ', yr))
+    }
+    
+    return(timeSince)
+  })
+  return(timeSinceYearly)
+}
