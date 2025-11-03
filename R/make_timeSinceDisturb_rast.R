26384f6599fdd24d99fed31ea68412c75912bee3
Julie Turner <julwturner@gmail.com>
2025-11-03 15:06:02 -0800

    update timeSinceDisturb fn

--- R/make_timeSinceDisturb_rast.R
+++ R/make_timeSinceDisturb_rast.R
@@ -3,25 +3,32 @@
 #' @author Julie W. Turner
 #' 
 #' 
-make_timeSinceDisturb_rast <- function(layer, rast, disturbanceType, minyr = NULL, backgrndYear = NULL, backgrndTS = NULL, where2save = NULL){
+make_timeSinceDisturb_rast <- function(layer, rast, disturbanceType, minyr = NULL, maxyr = NULL, backgrndYear = NULL, backgrndTS = NULL, where2save = NULL){
+  
   lyr <- layer
   raster <- rast
   yearsWithData <- unique(lyr[["YEAR"]])$YEAR
   if(!is.null(minyr)){
-    yearsWithData <- minyr:max(yearsWithData, na.rm = T)
+    if(!is.null(maxyr)){
+      yearsWithData <- minyr:maxyr
+    }else{
+      yearsWithData <- minyr:max(yearsWithData, na.rm = T)
+    }
+    
   }
   
   timeSinceYearly <- Map(yrName = paste0('year', yearsWithData),yr = yearsWithData, function(yrName, yr) {
     subst <- subset(lyr, lyr$YEAR<= yr)
-    print(paste0('start ', yrName))
+    message(paste0('start ', yrName))
     #fires <- 
     suppressWarnings({
-      historicalRasters <- rasterize(subst, raster, field="YEAR", fun=max, background = backgrndYear)
+      
+      historicalRasters <- terra::rasterize(subst, raster, field="YEAR", fun=max, background = backgrndYear)
       if(is.null(backgrndTS)){
-        timeSince <- yr - mask(historicalRasters, raster)
+        timeSince <- yr - terra::mask(historicalRasters, raster)
       } else{
         timeSince1 <- yr - historicalRasters
-        timeSince <- mask(ifel(is.na(timeSince1), backgrndTS, timeSince1), raster)
+        timeSince <- terra::mask(terra::ifel(is.na(timeSince1), backgrndTS, timeSince1), raster)
       }
       
       
@@ -30,11 +37,12 @@ make_timeSinceDisturb_rast <- function(layer, rast, disturbanceType, minyr = NUL
     
     if(!is.null(where2save)){
       fnames <- paste0("ts_", disturbanceType, "_", yr, ".tif")
-      writeRaster(timeSince, file.path(where2save, fnames), overwrite = T)
+      terra::writeRaster(timeSince, file.path(where2save, fnames), overwrite = T)
       print(paste0('saved ', disturbanceType, ' ', yr))
     }
     
     return(timeSince)
   })
+  message('finish yearly ', disturbanceType, ' rasterization')
   return(timeSinceYearly)
 }
